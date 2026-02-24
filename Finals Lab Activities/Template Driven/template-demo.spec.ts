39d1dec09a8dc2c4620fcda8e5c9f98b0fc04964
Luis Gabriel Narciso Aquino <thisisgab23@gmail.com>
2026-02-24 08:59:03 +0800

    Add files via upload

---
+++ Finals Lab Activities/Template Driven/template-demo.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { TemplateDemoComponent } from './template-demo';
+
+describe('TemplateDemoComponent', () => {
+  let component: TemplateDemoComponent;
+  let fixture: ComponentFixture<TemplateDemoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TemplateDemoComponent, FormsModule]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TemplateDemoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
